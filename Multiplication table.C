c467d11049c0dd6bb7fbe9646f57873808499fb2
Binayak2006-CSE <borris0609@gmail.com>
2026-08-10 23:38:55 +0530

    Add files via upload

---
+++ Multiplication table.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c,d,e,f,g,h,i,j;
+	a=7*1;
+	b=7*2;
+	c=7*3;
+	d=7*4;
+	e=7*5;
+	f=7*6;
+	g=7*7;
+	h=7*8;
+	i=7*9;
+	j=7*10;
+	printf("7x1=%d",a);
+	printf(" 7x2=%d",b);
+	printf(" 7x3=%d",c);
+	printf(" 7x4=%d",d);
+	printf(" 7x5=%d",e);
+	printf(" 7x6=%d",f);
+	printf(" 7x7=%d",g);
+	printf(" 7x8=%d",h);
+	printf(" 7x9=%d",i);
+	printf(" 7x10=%d",j);
+	return 0;
+	
+}
