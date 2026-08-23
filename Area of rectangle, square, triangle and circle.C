d956cffd403e091cf61750c4e6ac463012efa78c
Binayak Sinha <binayaksinha4@gmail.com>
2026-08-24 01:51:57 +0530

    Day 2

---
+++ Area of rectangle, square, triangle and circle.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	float r,A,a,b,c,C,d,e,f,R;
+	r=6.6;
+	R=3.14*r*r;
+	printf("The area of a circle is %f\n", R);
+	c=10.5;
+	C=c*c;
+	printf("The area of a square is %f\n", C);
+	d=12.5;
+	e=14;
+	f=((d*e)/2);
+	printf("The area of a triangle is %f\n", f);
+	a=12.5;
+	b=14.5;
+	A=a*b;
+	printf("The area of a rectangle is %f\n", A);
+	return 0;
+}
