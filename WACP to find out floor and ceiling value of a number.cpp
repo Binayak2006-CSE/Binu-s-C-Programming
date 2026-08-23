07a970374ed71c3c401f9d54fe19cab36fe6beb3
Binayak Sinha <binayaksinha4@gmail.com>
2026-08-24 01:56:22 +0530

    Day 2

---
+++ WACP to find out floor and ceiling value of a number.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float x;
+	x=8.5;
+	printf("The floor value of 'x' is %f\n", floor(x));
+	printf("The ceiling value of 'x' is %f\n", ceil(x));
+	return 0;
+}
